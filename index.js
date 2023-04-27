36293a8a3f99bbf638cfcbbb724ca4b4d0ccca87
Tim Sweeney-Fanelli <tim@zerobytellc.com>
2023-04-27 11:44:59 -0400

    Adding header comments

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+/*
+ * index.js
+ *
+ * Copyright (c) 2023 Zero Byte LLC, All Rights Reserved
+ * Licensed under CC BY-ND 4.0 (https://creativecommons.org/licenses/by-nd/4.0/)
+ *
+ * Module entry-point to export ZeroByteFW object. Methods exposed here are intended for public use. It is not
+ * recommended to use API methods exported from anywhere else, as they are considered to be internal and are
+ * subject to breaking changes without notice.
+ *
+ * SPDX-License-Identifier: CC-BY-ND-4.0
+ * SPDX-FileCopyrightText: Copyright (c) 2023 Zero Byte LLC (hello@zerobytellc.com) All Rights Reserved
+ *
+ * @author Timothy C Sweeney-Fanelli, Zero Byte LLC (tim@zerobytellc.com)
+ */
+
 import { get_latest_fw_info, retrieve_fw_index, download_fw }
     from './ZeroByteFirmwareUtils'
 
